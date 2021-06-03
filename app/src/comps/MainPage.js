43c8deae27a7d61fc0de2569f8a2b2c1a5becfd8
Ryan Miller <rozifus@gmail.com>
2021-06-04 00:38:24 +1000

    add basic material-ui

--- app/src/comps/MainPage.js
+++ app/src/comps/MainPage.js
@@ -1,6 +1,25 @@
 import React, { useState, useRef, useEffect } from "react";
-import { newContextComponents } from "@drizzle/react-components";
 import ierc721json from "../contracts/IERC721.json";
+
+import TextField from '@material-ui/core/TextField';
+import FilledInput from '@material-ui/core/FilledInput';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Done from '@material-ui/icons/Done';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  margin: {
+    margin: theme.spacing(1)
+  },
+}));
 
 const usePrevious = value => {
   const ref = useRef();
@@ -13,7 +32,10 @@ const usePrevious = value => {
 } 
 
 export default ({drizzle, drizzleState}) => {
+  const classes = useStyles();
+
   const [contract, setContract] = useState("");
+  const validContract = drizzle.web3.utils.isAddress(contract);
   const prevContract = usePrevious(contract);
   const contractDrizzled = Boolean(drizzle.contracts[contract]);
 
@@ -22,7 +44,7 @@ export default ({drizzle, drizzleState}) => {
       drizzle.deleteContract(prevContract);
     }
 
-    if (drizzle.web3.utils.isAddress(contract)) {
+    if (validContract) {
       const config = {
         contractName: contract,
         web3Contract: new drizzle.web3.eth.Contract(ierc721json.abi, contract)
@@ -31,18 +53,31 @@ export default ({drizzle, drizzleState}) => {
     }
   }, [contract])
  
-  const style = {
+  const contractStyle = {
     color: contractDrizzled ? "green" : "red"
   }
 
+  const validIcon = validContract ? (
+    <InputAdornment position="start">
+      <IconButton edge="start">
+        <Done />
+      </IconButton>
+    </InputAdornment>
+  ) : null
+
   return (
-    <div className="section">
-      <h2>Metadata Viewer</h2>
-      <input type="text" value={contract} onChange={(e) => setContract(e.target.value)} />
-      <p>
-        <strong>Contract </strong>
-        <span style={style}>{contract}</span>
-      </p>
+    <div className={classes.root}>
+      <FormControl fullWidth className={classes.margin}>
+        <TextField
+          label="contract"
+          variant="outlined"
+          value={contract}
+          onChange={(e) => setContract(e.target.value && e.target.value.trim())}
+          InputProps={{
+            startAdornment: validIcon
+          }}
+        />
+      </FormControl>
     </div>
   );
 };
